ebd30fb7ecc36b7a3ce0bc4428778aca03bb42a3
Shakshor <shakshor584@gmail.com>
2022-04-13 16:26:52 +0600

    adding website live link

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './NotFound.css';
-// import '../../images/bg.jpg';
+import '../../images/bg.jpg';
 
 const NotFound = () => {
     const navigate = useNavigate();
